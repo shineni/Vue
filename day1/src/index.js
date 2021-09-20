ca48bd3c370f1ddd84db4fcdd60bdd82fd9c4103
niyingying <928514062@qq.com>
2021-09-20 23:35:56 +0800

    vue baic concept

--- day1/src/index.js
+++ day1/src/index.js
@@ -1,6 +1,7 @@
 import "@/css/index.css"  // 导入样式（webpack 中一切皆模块，都可以用ES6导入语法进行导入） 如果用from接受到的成员返回的是undefined,则没有必要接受
 import "@/css/index.less" 
 import logo from "@/images/logo.jpg"
+import Vue from 'vue'
 //导入的是一个路径，导出的是一个base64的字符串，这样可以减少请求
 
 // let el = document.getElementById('main')
@@ -17,3 +18,12 @@ function info(targert) {
 class Person{}
 //4. 打印Person的静态属性info
 console.log(Person.info)
+
+
+const vm = new Vue({
+    el:'#app',  // el属性，vm实例控制的区域
+    data:{  // data就是要渲染到页面的数据
+
+    }
+
+})
